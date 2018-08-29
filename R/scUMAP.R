2f84a7a8645749709b00c6064684a26fb3a9dd79
Jonathan Augustin <jaugust7@jhmi.edu>
2018-08-29 14:57:20 -0400

    fix scUMAP function

--- R/scUMAP.R
+++ R/scUMAP.R
@@ -12,13 +12,19 @@
 #' @examples
 #' 
 
-scUMAP<-function(cds, nComp=NULL, cell_color=NULL, scale_color=c("#377EB8", "#4DAF4A", "#E41A1C")){
-  tmp<-umap(prcomp_irlba(log(t(exprs(cds[row.names(subset(fData(cds), use_for_ordering == TRUE))])+1)), n = tail(nComp, n=1))$x[,nComp]) 
+scUMAP<-function(cds, nComp=NULL, cell_color="color", scale_color=c("#377EB8", "#4DAF4A", "#E41A1C")){
+  x<-cds
+  tmp<-prcomp_irlba(log(t(exprs(x[row.names(subset(fData(x), use_for_ordering == TRUE))])+1)), n = tail(nComp, n=1))
+  tmp1<-umap(tmp$x[,nComp]) 
   
-  pData(cds)$UMAP1<-tmp$layout[,1]
-  pData(cds)$UMAP2<-tmp$layout[,2]
+  pData(x)$UMAP1<-tmp1$layout[,1]
+  pData(x)$UMAP2<-tmp1$layout[,2]
   
-  tmp2<-ggplot(pData(cds)) + geom_point(aes(x=UMAP1, y=UMAP2, color=pData(cds)$cell_color), size=1, alpha=0.5) + theme_bw() + scale_color_manual(values=scale_color) + labs(x="UMAP 1", y ="UMAP 2", color = "cell_color") + monocle:::monocle_theme_opts()
-  return(tmp2) 
+  p<-ggplot(pData(x), aes(x=UMAP1, y=UMAP2))
+  p + geom_point(aes_string(color=cell_color), size=1, alpha=0.5) +
+    theme_bw() + 
+    scale_color_manual(values=scale_color) + 
+    labs(x="UMAP 1", y ="UMAP 2", color = "cell_color") + 
+    monocle:::monocle_theme_opts()
 }
 
