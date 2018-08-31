9dbf6660af32b09ffe348b9801942cfde8e458f1
Jonathan Augustin <jaugust7@jhmi.edu>
2018-08-31 08:57:52 -0400

    Add new functions: cordUMAP and cordNormUMAP

---
+++ R/cordNormUMAP.R
@@ -0,0 +1,23 @@
+#' Generate new cds with UMAP coordinates in pData
+#' 
+#' @description Generate 2D UMAP coordinates based on count normalized expression matrix and append to CDS
+#' 
+#' @param cds a cell data set
+#' @param nComp a vector consisting of the number of PCs to be used
+#' 
+#' @export
+#' 
+#' @return Returns a CDS with UMAP coordinates in pData
+#' 
+#' @examples
+#' 
+
+cordNormUMAP<-function (cds, nComp = NULL) 
+{
+  x <- cds
+  tmp <- prcomp_irlba(t(scnorm(x[row.names(subset(fData(x), use_for_ordering == TRUE))])), n = tail(nComp, n = 1))
+  tmp1 <- umap(tmp$x[, nComp])
+  pData(x)$UMAP1 <- tmp1$layout[, 1]
+  pData(x)$UMAP2 <- tmp1$layout[, 2]
+  return(x)
+}
---
+++ R/cordUMAP.R
@@ -0,0 +1,23 @@
+#' Generate new cds with UMAP coordinates in pData
+#' 
+#' @description Generate a 2D UMAP coordinates and append to CDS
+#' 
+#' @param cds a cell data set
+#' @param nComp a vector consisting of the number of PCs to be used
+#' 
+#' @export
+#' 
+#' @return Returns a CDS with UMAP coordinates in pData
+#' 
+#' @examples
+#' 
+
+cordUMAP<-function(cds, nComp=NULL){
+  x<-cds
+  tmp<-prcomp_irlba(log(t(as.matrix(exprs(x[row.names(subset(fData(x), use_for_ordering == TRUE))]))+1)), n = tail(nComp, n=1))
+  tmp1<-umap(tmp$x[,nComp]) 
+  
+  pData(x)$UMAP1<-tmp1$layout[,1]
+  pData(x)$UMAP2<-tmp1$layout[,2]
+  return(x)
+}
