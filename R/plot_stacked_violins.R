50a1394a431f64e2460c7ae843b002bec532ba28
Jonathan Augustin <jaugust7@jhmi.edu>
2022-12-06 10:06:33 -0800

    Update plot_stacked_violins.R

--- R/plot_stacked_violins.R
+++ R/plot_stacked_violins.R
@@ -12,16 +12,18 @@
 #' 
 #' 
 plot_stacked_violins<-function(cds = NULL, genes = c("NULL"), group_by = "cluster", assay_name = "counts" ){
-  cds<-cds[monocle3::fData(cds)$gene_short_name %in% genes]
-  features<-unique(monocle3::fData(cds)$gene_short_name)
+  cds<-cds[fData(cds)$gene_short_name %in% genes,]
+  features<-unique(fData(cds)$gene_short_name)
   list<-lapply(features, function(x){
-    tmp<-cds[monocle3::fData(cds)$gene_short_name == x,]
-    data.frame("Cell" = colnames(tmp), "Idents" = as.factor(monocle3::pData(tmp)[[group_by]]), "Feat" = rep(x), "Expr" = exprs(tmp)[1,])
+    tmp<-cds[fData(cds)$gene_short_name == x,]
+    data.frame("Cell" = colnames(tmp), "Idents" = as.factor(pData(tmp)[[group_by]]), "Feat" = rep(x), "Expr" = exprs(tmp)[1,])
   })
   tmp<-do.call(rbind, list)
   tmp$Expr<-log2(tmp$Expr + 0.1)
+  
   avg <- sapply(X = split(x = tmp, f = tmp$Idents),
                 FUN = function(df) { return(tapply(X = df$Expr, INDEX = df$Feat, FUN = mean)) })
+  
   L2Norm <- function(mat, MARGIN){
     normalized <- sweep(x = mat, MARGIN = MARGIN,
                         STATS = apply(X = mat, MARGIN = MARGIN,
@@ -32,7 +34,8 @@ plot_stacked_violins<-function(cds = NULL, genes = c("NULL"), group_by = "cluste
   idents.order <- hclust(d = dist(t(L2Norm(mat = avg, MARGIN = 2))))$order
   avg <- avg[,idents.order]
   avg <- L2Norm(mat = avg, MARGIN = 1)
-  mat <- stats::hclust(d = stats::dist(avg))$merge
+  mat <- hclust(d = dist(avg))$merge
+  
   position <- apply(X = avg, MARGIN = 1, FUN = which.max)
   orderings <- list()
   for (i in 1:nrow(mat)) {
@@ -46,17 +49,18 @@ plot_stacked_violins<-function(cds = NULL, genes = c("NULL"), group_by = "cluste
   tmp$Idents <- factor(tmp$Idents, levels = levels(tmp$Idents)[idents.order])
   tmp$Feat <- as.factor(tmp$Feat)
   tmp$Feat <- factor(tmp$Feat, levels = levels(tmp$Feat)[features.order])
-  ggplot2::ggplot(tmp, aes(factor(Idents), Expr, fill = Idents)) +
-    ggplot2::geom_violin(scale = "width", adjust = 1, trim = TRUE) +
-    ggplot2::scale_y_continuous(expand = c(0, 0), position="right", labels = function(x)
+  
+  ggplot(tmp, aes(factor(Idents), Expr, fill = Idents)) +
+    geom_violin(scale = "width", adjust = 1, trim = TRUE) +
+    scale_y_continuous(expand = c(0, 0), position="right", labels = function(x)
       c(rep(x = "", times = length(x)-2), x[length(x) - 1], "")) +
-    ggplot2::facet_grid(rows = vars(Feat), scales = "free", switch = "y") +
+    facet_grid(rows = vars(Feat), scales = "free", switch = "y") +
     cowplot::theme_cowplot(font_size = 12) +
-    ggplot2::theme(legend.position = "none", panel.spacing = unit(0, "lines"),
+    theme(legend.position = "none", panel.spacing = unit(0, "lines"),
           panel.background = element_rect(fill = NA, color = "black"),
           strip.background = element_blank(),
           strip.text = element_text(face = "bold"),
           strip.text.y.left = element_text(angle = 0),
           axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
-    ggplot2::xlab(group_by) + ylab("Expression Level")
+    xlab(group_by) + ylab("Expression Level")
 }
